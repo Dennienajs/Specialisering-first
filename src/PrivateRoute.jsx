1509ae42fd1ff22ce5ddcde013ad7453c143fd10
Dennienajs <dennie.najs@hotmail.com>
2019-11-23 15:48:05 +0100

    Added PrivateRoute component for authentication

---
+++ src/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { AuthContext } from "./context";
+
+// Hvilken route som skal renders, hvis der er en auththenticated user (Content component)
+const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
+  const currentUser = useContext(AuthContext);
+  return (
+    <Route
+      {...rest}
+      render={routeProps =>
+        !!currentUser ? (
+          <RouteComponent {...routeProps} />
+        ) : (
+          <Redirect to={"/login"} />
+        )
+      }
+    />
+  );
+};
+
+// Fjern det ene "!" i "!!currentUser" for kun at kunne bruge appen hvis du er logget ind.
+
+export default PrivateRoute;
