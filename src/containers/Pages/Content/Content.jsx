060ae65c90e0b356199a17093a7e601ae3f4f2d6
Dennienajs <dennie.najs@hotmail.com>
2019-11-23 15:46:27 +0100

    Content moved to Pages folder

---
+++ src/containers/Pages/Content/Content.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../../Sidebar";
+import Punkter from "../../../components/Punkter";
+
+const Content = () => {
+  return (
+    <section className="content">
+      <Sidebar />
+      <Punkter />
+    </section>
+  );
+};
+
+export default Content;
