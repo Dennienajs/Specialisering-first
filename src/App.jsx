950a26b87879514eb1fcb2e4734b7e13cdbde97c
Dennienajs <dennie.najs@hotmail.com>
2019-11-23 15:48:57 +0100

    Added login, signup routes + Auth Context for auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
 import React from "react";
 import "./App.scss";
-import Content from "./containers/Content/index";
-import Header from "./containers/Header/index";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ListerProvider, ValgtListeProvider } from "./context";
+import Header from "./containers/Header/index";
+import Login from "./containers/Pages/Login";
+import Signup from "./containers/Pages/Signup";
+import { AuthProvider } from "./context";
+import PrivateRoute from "./PrivateRoute";
+import Content from "./containers/Pages/Content";
 
 export const App = () => {
   return (
     <ValgtListeProvider>
       <ListerProvider>
-        <main className="App" data-testid="application">
-          <Header />
-          <Content />
-        </main>
+        <AuthProvider>
+          <main className="App" data-testid="application">
+            <Router>
+              <Header />
+              <PrivateRoute exact path="/" component={Content} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+            </Router>
+          </main>
+        </AuthProvider>
       </ListerProvider>
     </ValgtListeProvider>
   );
 };
 
+// ************
 /*
+AuthProvider giver access til "currentUser" gennem Context API
+ - 
 
-Online at:
-https://specialisering-1st.dennienajs.now.sh/
-
-to update:
-now --prod
-
+now --prod  // updates: https://specialisering-1st.dennienajs.now.sh/
+npm run test -- --coverage  // Tests
 */
-
-// npm run test -- --coverage
