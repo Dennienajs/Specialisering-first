6842908fb6864bffd6c702d017f8aa54d3660cf0
Dennienajs <dennie.najs@hotmail.com>
2019-11-24 14:22:40 +0100

    added toggle sidebar open/close

--- src/containers/Pages/Content/Content.jsx
+++ src/containers/Pages/Content/Content.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../../Sidebar";
 import Punkter from "../../../components/Punkter";
 
 const Content = () => {
+  const [visSidebar, setVisSidebar] = useState(true);
+
   return (
     <section className="content">
-      <Sidebar />
-      <Punkter />
+      <div className="content__toggle-sidebar">
+        <label className="switch">
+          <input
+            type="checkbox"
+            value={visSidebar}
+            onClick={() => setVisSidebar(!visSidebar)}
+            checked={visSidebar}
+          />
+          <span className="slider"></span>
+        </label>
+      </div>
+      {visSidebar ? <Sidebar /> : null}
+      <Punkter visSidebar={visSidebar} setVisSidebar={setVisSidebar} />
     </section>
   );
 };
