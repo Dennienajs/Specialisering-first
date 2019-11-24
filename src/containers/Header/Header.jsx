5761918a5fc20b12ea188475c42ee1a045ac2066
Dennienajs <dennie.najs@hotmail.com>
2019-11-24 19:21:28 +0100

    added email verification + display name from email

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { FaRegMoon, FaPlus, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { firebase } from "../../firebase";
 import { Link } from "react-router-dom";
@@ -6,7 +6,63 @@ import { AuthContext } from "../../context";
 
 const Header = () => {
   const { currentUser } = useContext(AuthContext);
-  console.log(currentUser ? "true" : "no user");
+  console.log(
+    currentUser
+      ? currentUser.displayName
+        ? currentUser.displayName
+        : currentUser.email
+      : "no user"
+  );
+
+  // HANDLES EMAIL VERIFICATION
+  useEffect(() => {
+    if (currentUser && !currentUser.emailVerified) {
+      // Alert popop confirmation.
+      let confirmation = window.confirm(
+        "You email is not verified. Send verification email to " +
+          currentUser.email
+      );
+      // Hvis man accepterer popop, sendes verification email.
+      confirmation
+        ? currentUser
+            .sendEmailVerification()
+            .then(() => {
+              alert("Verification email sent to: " + currentUser.email);
+              console.log("Verification email sent to: " + currentUser.email);
+            })
+            .catch(err => {
+              console.error(err);
+              console.log("Error sending verification email");
+            })
+        : console.log("currentUser: " + currentUser);
+    }
+  }, [currentUser]);
+
+  // Hvis brugeren intet displayName har, får man et ud fra ens email.
+  useEffect(() => {
+    const getProfileData = () => {
+      if (currentUser && !currentUser.displayName) {
+        let emailDisplay = currentUser.email.split("@", 1);
+        currentUser
+          .updateProfile({
+            displayName: emailDisplay.toString()
+          })
+          .catch(err => console.error(err));
+        console.log(currentUser.displayName);
+      }
+    };
+  }, [currentUser]);
+
+  // Til debug og test. Tjekker om email er verified.
+  const handleOnClickDisplayName = () => {
+    if (currentUser && !currentUser.emailVerified) {
+      console.log("email not verified");
+    }
+    if (currentUser && currentUser.emailVerified) {
+      console.log("email is verified");
+    }
+  };
+
   return (
     <header className="header" data-testid="header">
       <nav>
@@ -19,17 +75,21 @@ const Header = () => {
         <div className="settings">
           <ul>
             <li className="settings-add">
-              <button>
-                {currentUser ? currentUser.email : "Not signed in"}
+              <button onClick={() => handleOnClickDisplayName()}>
+                {currentUser
+                  ? currentUser.displayName
+                    ? currentUser.displayName
+                    : currentUser.email
+                  : "Not signed in"}
               </button>
             </li>
             <li className="settings-add">
-              <button>
+              <button onClick={() => console.log("TODO: something..")}>
                 <FaPlus />
               </button>
             </li>
             <li className="settings-darkmode">
-              <button>
+              <button onClick={() => console.log("TODO: darkmode")}>
                 <FaRegMoon />
               </button>
             </li>
@@ -45,7 +105,9 @@ const Header = () => {
                   data-testid="header-button-signout"
                   onClick={() => {
                     firebase.auth().signOut();
-                    console.log("Sign out");
+                    localStorage.removeItem("FBIdToken");
+                    console.log("Signed out");
+                    console.log("FBIdToken removed");
                   }}
                 >
                   <FaSignOutAlt />
