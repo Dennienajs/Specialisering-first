f9ca14d872c48a7726e8b629e52bc8e23fe8595e
Dennienajs <dennie.najs@hotmail.com>
2019-11-25 11:09:03 +0100

    Toggle sidebar moved/added

---
+++ src/containers/Pages/Content/ButtonToggleSidebar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ButtonToggleSidebar = ({ visSidebar, setVisSidebar }) => {
+  return (
+    <div className="content__toggle-sidebar">
+      <label className="switch">
+        <input
+          type="checkbox"
+          value={visSidebar}
+          onClick={() => setVisSidebar(!visSidebar)}
+          checked={visSidebar}
+        />
+        <span className="slider"></span>
+      </label>
+    </div>
+  );
+};
+
+export default ButtonToggleSidebar;
--- src/containers/Pages/Content/Content.jsx
+++ src/containers/Pages/Content/Content.jsx
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
 import Sidebar from "../../Sidebar";
 import Punkter from "../../../components/Punkter";
+import ButtonToggleSidebar from "./ButtonToggleSidebar";
 
 const Content = () => {
   const [visSidebar, setVisSidebar] = useState(true);
-
+  // console.log("darkmode content: " + darkMode);
+  // className = { darkMode? "content darkmode": "content" }
   return (
-    <section className="content" data-testid="content">
-      <div className="content__toggle-sidebar">
-        <label className="switch">
-          <input
-            type="checkbox"
-            value={visSidebar}
-            onClick={() => setVisSidebar(!visSidebar)}
-            checked={visSidebar}
-          />
-          <span className="slider"></span>
-        </label>
-      </div>
+    <section data-testid="content">
+      <ButtonToggleSidebar
+        visSidebar={visSidebar}
+        setVisSidebar={setVisSidebar}
+      />
       {visSidebar ? <Sidebar /> : null}
       <Punkter visSidebar={visSidebar} />
     </section>
