6445c54140a80085e62c8e0106aa05479775161d
Dennienajs <dennie.najs@hotmail.com>
2019-11-25 19:10:19 +0100

    added test, doesnt work tho

--- src/__tests__/PrivateRoute.spec.js
+++ src/__tests__/PrivateRoute.spec.js
@@ -1,1 +1,17 @@
-// LATER
+import React from "react";
+import PrivateRoute from "../PrivateRoute";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+it("render  PrivateRoute", () => {
+  const { queryByTestId } = render(
+    <Router>
+      <PrivateRoute />
+    </Router>
+  );
+  expect(queryByTestId("private-route")).toBeTruthy();
+});
+
+// Fik aldrig denne til at virke.
+// Hvad skal jeg expect?
