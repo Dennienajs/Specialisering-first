c441c47c66b6948a3cc375e9ffd5b2dfe37cd911
Dennienajs <dennie.najs@hotmail.com>
2019-11-25 17:31:33 +0100

    fixed to look like default sidebar list

--- src/components/IndividuelListe/IndividuelListe.jsx
+++ src/components/IndividuelListe/IndividuelListe.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
-// import { useListerValue, useValgtListeValue } from "../../context";
 
 const IndividuelListe = ({ liste }) => {
-  // const { lister, setLister } = useListerValue();
-  // const { setValgtListe } = useValgtListeValue();
-
   return (
-    <div className="individuel-liste-sidebar">
-      <span className="individuel-liste-sidebar__star">
+    <>
+      <span>
         <FaStar />
       </span>
-      <span className="individuel-liste-sidebar__liste-navn">{liste.navn}</span>
-    </div>
+      <span>{liste.navn}</span>
+    </>
   );
 };
 
