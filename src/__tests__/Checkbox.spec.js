b4f333667c1ad6fe322f01da05025abe0f43cb73
Dennienajs <dennie.najs@hotmail.com>
2019-11-26 13:08:47 +0100

    Updated to 100% coverage

--- src/__tests__/Checkbox.spec.js
+++ src/__tests__/Checkbox.spec.js
@@ -1,29 +1,13 @@
 import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import Checkbox from "../components/Checkbox";
-
 // To run: npm run test -- --coverage
-
 // Jest testing with react-testing-library
 // Vi har ikke haft om testing i React før, så jeg starter ud med meget simple tests.
 // Der er ikke fokus på hvad der bliver testet, men læringsprocessen bag.
-
 beforeEach(cleanup); // Cleans the DOM
 
-// Mocks firebase med fake functions og alle functionerne som bruges i firebase.js filen.
-// Vi skal bruge firebase i testen, men vil ikke at den kalder til den rigtige db.
-
-// jest.mock("../firebase", () => ({
-//   firebase: {
-//     firestore: jest.fn(() => ({
-//       collection: jest.fn(() => ({
-//         doc: jest.fn(() => ({
-//           update: jest.fn()
-//         }))
-//       }))
-//     }))
-//   }
-// }));
+// UPDATED TO 100% COVERAGE 26-11-2019, 13:00.
 
 describe("<Checkbox />", () => {
   describe("Success", () => {
@@ -34,22 +18,94 @@ describe("<Checkbox />", () => {
       expect(queryByTestId("checkbox-action")).toBeTruthy(); // data-testid="checkbox-action"
     });
 
-    // onClick
-    it("renders the punkter checkbox and accepts an onClick", () => {
+    // onClick, DONE (line-through)
+    it("renders checkbox type=done and accepts an onClick", () => {
       const { queryByTestId } = render(
-        <Checkbox id="1" indhold="'Make great testing!'" />
+        <Checkbox
+          id="1"
+          indhold="'Make great testing!'"
+          type="done"
+          arkiveret="false"
+        />
       );
       expect(queryByTestId("checkbox-action")).toBeTruthy();
       fireEvent.click(queryByTestId("checkbox-action"));
+      expect(queryByTestId("checkbox-tick")).toBeTruthy();
     });
 
-    // onKeyDown
-    it("renders the punkter checkbox and accepts an onKeyDown", () => {
+    // onKeyDown DONE (line-through)
+    it("renders checkbox type=done and accepts an onKeyDown", () => {
       const { queryByTestId } = render(
-        <Checkbox id="1" indhold="'Make great testing!'" />
+        <Checkbox
+          id="1"
+          indhold="'Make great testing!'"
+          type="done"
+          arkiveret="false"
+        />
+      );
+      expect(queryByTestId("checkbox-action")).toBeTruthy();
+      fireEvent.keyDown(queryByTestId("checkbox-action"));
+      expect(queryByTestId("checkbox-tick")).toBeTruthy();
+    });
+
+    // onClick DELETE (line-through)
+    it("renders checkbox type=delete and accepts an onClick", () => {
+      const { queryByTestId } = render(
+        <Checkbox
+          id="1"
+          indhold="'Make great testing!'"
+          type="delete"
+          arkiveret="false"
+        />
+      );
+      expect(queryByTestId("checkbox-action")).toBeTruthy();
+      fireEvent.click(queryByTestId("checkbox-action"));
+      expect(queryByTestId("checkbox-delete")).toBeTruthy();
+      expect(queryByTestId("checkbox-tick")).toBeFalsy();
+    });
+
+    // onKeyDown DELETE (line-through)
+    it("renders checkbox type=delete and accepts an onKeyDown", () => {
+      const { queryByTestId } = render(
+        <Checkbox
+          id="1"
+          indhold="'Make great testing!'"
+          type="delete"
+          arkiveret="false"
+        />
       );
       fireEvent.keyDown(queryByTestId("checkbox-action"));
       expect(queryByTestId("checkbox-action")).toBeTruthy();
+    });
+
+    // onKeyDown DELETE (line-through)
+    it("renders checkbox type=somethingElse and accepts an onClick", () => {
+      const { queryByTestId } = render(
+        <Checkbox
+          id="1"
+          indhold="'Make great testing!'"
+          type="somethingElse"
+          arkiveret="false"
+        />
+      );
+      fireEvent.click(queryByTestId("checkbox-action"));
+      expect(queryByTestId("checkbox-delete")).toBeFalsy();
+      expect(queryByTestId("checkbox-tick")).toBeFalsy();
+    });
+
+    // onKeyDown DELETE (line-through)
+    it("renders checkbox type=somethingElse and accepts an onKeyDown", () => {
+      const { queryByTestId } = render(
+        <Checkbox
+          id="1"
+          indhold="'Make great testing!'"
+          type="somethingElse"
+          arkiveret="false"
+        />
+      );
+      fireEvent.keyDown(queryByTestId("checkbox-action"));
+      expect(queryByTestId("checkbox-delete")).toBeFalsy();
+      expect(queryByTestId("checkbox-tick")).toBeFalsy();
     });
   });
 });
