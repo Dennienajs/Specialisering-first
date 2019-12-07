7a67d7c5946a38e4878f8d74d80f06fcad3fc0f0
Dennienajs <dennie.najs@hotmail.com>
2019-12-07 20:25:45 +0100

    added explicit any

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -3,7 +3,7 @@ import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "./context";
 
 // Hvilken route som skal renders, hvis der er en auththenticated user (Content component)
-export const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
+export const PrivateRoute = ({ component: RouteComponent, ...rest }: any) => {
   const currentUser = useContext(AuthContext);
   return (
     <Route
