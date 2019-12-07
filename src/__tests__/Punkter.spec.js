389816600c9f78e7bdb17e248c8090b4f9998e01
Dennienajs <dennie.najs@hotmail.com>
2019-12-07 20:52:13 +0100

    100% coverage

--- src/__tests__/Punkter.spec.js
+++ src/__tests__/Punkter.spec.js
@@ -3,102 +3,138 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import Punkter from "../components/Punkter";
 import {
-  useValgtListeValue,
-  useListerValue,
+  ListerContext,
+  ValgtListeContext,
   AuthContext,
-  AuthProvider
+  ThemeContext
 } from "../context";
-
-// Context mock - useValgtListeValue, useListerValue
-jest.mock("../context", () => ({
-  useValgtListeValue: jest.fn(),
-  useListerValue: jest.fn(() => ({
-    Lister: [
-      {
-        navn: "001",
-        listeId: "1",
-        brugerId: "jlIFXIwyAL3tzHMtzRbw",
-        docId: "wakeup"
-      },
-      {
-        navn: "002",
-        listeId: "2",
-        brugerId: "jlIFXIwyAL3tzHMtzRbw",
-        docId: "stress"
-      },
-      {
-        navn: "003",
-        listeId: "3",
-        brugerId: "jlIFXIwyAL3tzHMtzRbw",
-        docId: "remember"
-      },
-      {
-        navn: "004",
-        listeId: "4",
-        brugerId: "jlIFXIwyAL3tzHMtzRbw",
-        docId: "this"
-      },
-      {
-        navn: "005",
-        listeId: "5",
-        brugerId: "jlIFXIwyAL3tzHMtzRbw",
-        docId: "NF"
-      }
-    ]
-  }))
-}));
 
 // Hooks mocks - usePunkter
 jest.mock("../hooks", () => ({
   usePunkter: () => ({
     punkter: [
       {
-        id: "mx2taaXpF38vYqMGbVtY",
-        arkiveret: false,
-        dato: "21/07/2019",
-        listeId: "1",
-        punkt: "Learn some Jest testing or die tryin'",
-        userId: "1234567890"
+        // id: "mx2taaXpF38vYqMGbVtY",
+        // arkiveret: false,
+        // dato: "21/07/2019",
+        // listeId: "1",
+        // punkt: "Learn some Jest testing or die tryin'",
+        // userId: "1234567890"
       }
-    ]
+    ],
+    loadingPunkter: false
   })
 }));
 
-// MOCK END *****
-
 beforeEach(cleanup);
 
-// TEST START *****
 describe("<Punkter />", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 });
 
-// Alle punkter
-it("render ALLE punkter", () => {
-  useValgtListeValue.mockImplementation(() => ({
-    setvalgtListe: jest.fn(() => "Alle"),
-    valgtListe: "ALLE" // "" = "ALLE"
-  }));
+// punkter + currentUser.displayName=true
+it("render <Punkter /> MED currentUser MED displayName", () => {
+  const theme = {};
+  const dark = true;
+  const toggle = jest.fn();
+  const lister = [{}];
+  const valgtListe = "IDAG";
+  const currentUser = {
+    email: "user@email.com",
+    updateProfile: jest.fn(),
+    displayName: "user"
+  };
+
+  const { queryByTestId } = render(
+    <AuthContext.Provider value={{ currentUser }}>
+      <ListerContext.Provider value={{ lister }}>
+        <ValgtListeContext.Provider value={{ valgtListe }}>
+          <ThemeContext.Provider value={{ theme, dark, toggle }}>
+            <Punkter />
+          </ThemeContext.Provider>
+        </ValgtListeContext.Provider>
+      </ListerContext.Provider>
+    </AuthContext.Provider>
+  );
+  expect(queryByTestId("tilføj-punkt")).toBeTruthy();
+  expect(queryByTestId("punkter")).toBeTruthy();
+  expect(queryByTestId("liste-navn").textContent).toBe("IDAG"); // const valgtListe = "IDAG";
+});
+
+// punkter + currentUser.displayName=false
+it("render <Punkter /> MED currentUser UDEN displayName", () => {
+  const theme = {};
+  const dark = true;
+  const toggle = jest.fn();
+  const lister = [{}];
+  const valgtListe = "IDAG";
+  const currentUser = {
+    email: "user@email.com",
+    updateProfile: jest.fn()
+  };
+
+  const { queryByTestId } = render(
+    <AuthContext.Provider value={{ currentUser }}>
+      <ListerContext.Provider value={{ lister }}>
+        <ValgtListeContext.Provider value={{ valgtListe }}>
+          <ThemeContext.Provider value={{ theme, dark, toggle }}>
+            <Punkter />
+          </ThemeContext.Provider>
+        </ValgtListeContext.Provider>
+      </ListerContext.Provider>
+    </AuthContext.Provider>
+  );
+  expect(queryByTestId("tilføj-punkt")).toBeTruthy();
+  expect(queryByTestId("punkter")).toBeTruthy();
+  expect(queryByTestId("liste-navn").textContent).toBe("IDAG"); // const valgtListe = "IDAG";
+});
+
+// punkter + currentUser.displayName=true
+it("render <Punkter /> UDEN currentUser", () => {
+  const theme = {};
+  const dark = true;
+  const toggle = jest.fn();
+  const lister = [{}];
+  const valgtListe = "IDAG";
 
   const { queryByTestId } = render(
     <AuthContext.Provider value={{}}>
-      <Punkter />
+      <ListerContext.Provider value={{ lister }}>
+        <ValgtListeContext.Provider value={{ valgtListe }}>
+          <ThemeContext.Provider value={{ theme, dark, toggle }}>
+            <Punkter />
+          </ThemeContext.Provider>
+        </ValgtListeContext.Provider>
+      </ListerContext.Provider>
     </AuthContext.Provider>
   );
+  expect(queryByTestId("tilføj-punkt")).toBeTruthy();
   expect(queryByTestId("punkter")).toBeTruthy();
-  expect(queryByTestId("liste-navn").textContent).toBe("ALLE");
+  expect(queryByTestId("liste-navn").textContent).toBe("IDAG"); // const valgtListe = "IDAG";
 });
 
-// Idag punkter (for at ramme 100 coverage.)
-it("render idag punkter", () => {
-  useValgtListeValue.mockImplementation(() => ({
-    setvalgtListe: jest.fn(() => "Idag"),
-    valgtListe: "IDAG" // "" = "ALLE"
-  }));
+// Alle ("") punkter
+it("render <Punkter /> listeNavn '' (alle punkter) ", () => {
+  const theme = {};
+  const dark = true;
+  const toggle = jest.fn();
+  const lister = [{}];
+  const valgtListe = ""; // = ALLE
 
-  const { queryByTestId } = render(<Punkter />);
+  const { queryByTestId } = render(
+    <AuthContext.Provider value={{}}>
+      <ListerContext.Provider value={{ lister }}>
+        <ValgtListeContext.Provider value={{ valgtListe }}>
+          <ThemeContext.Provider value={{ theme, dark, toggle }}>
+            <Punkter />
+          </ThemeContext.Provider>
+        </ValgtListeContext.Provider>
+      </ListerContext.Provider>
+    </AuthContext.Provider>
+  );
+  expect(queryByTestId("tilføj-punkt")).toBeTruthy();
   expect(queryByTestId("punkter")).toBeTruthy();
-  expect(queryByTestId("liste-navn").textContent).toBe("IDAG");
+  expect(queryByTestId("liste-navn").textContent).toBe("ALLE"); // const valgtListe = "ALLE";
 });
