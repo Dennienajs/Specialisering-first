797a433b9dd8c9755f9dd38c8e1e62e8af4506b0
Dennienajs <dennie.najs@hotmail.com>
2019-12-10 19:19:01 +0100

    clean

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -2,11 +2,12 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import { App } from "../App";
 
-beforeEach(cleanup); // Cleans the dom
-
 // Tester om applicationen render.
 // Selvom det virker dumt at teste dette, giver det virkelig meget test coverage ->
 // Sammenlign evt billede 1 og 2 i './coverage-progress-pics'
+
+beforeEach(cleanup); // Cleans the dom
+
 describe("<App />", () => {
   it("renders the application", () => {
     const { queryByTestId } = render(<App />);
