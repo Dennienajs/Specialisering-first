5310db77a12f4abb3044f80206de1dd5d7ebeecd
DennieChristensen <44695505+Dennienajs@users.noreply.github.com>
2019-12-13 11:17:48 +0100

    slight adjustments

--- src/__tests__/TilføjPunkt.spec.js
+++ src/__tests__/TilføjPunkt.spec.js
@@ -16,11 +16,13 @@ beforeEach(cleanup);
 // NOTE: HVORDAN RAMMER JEG CATCH MED MOCKEN? Jeg kan sagtens ramme én af resolve/reject,
 // men ikke dem begge når mocken er sat her? Kan ændre forneden, men ikke ramme begge samtidigt i to forskellige tests???
 // MOCK "tilføjPunkt" - firebase query, add punkt.
+
 jest.mock("../firebase", () => ({
   firebase: {
     firestore: jest.fn(() => ({
       collection: jest.fn(() => ({
-        add: jest.fn(() => Promise.resolve("Promise resolved .."))
+        add: jest.fn(() => Promise.resolve())
+        // add: jest.fn(() => Promise.reject("Promise rejected .."))
       }))
     }))
   }
@@ -53,8 +55,8 @@ describe("<TilføjPunkt />", () => {
       expect(queryByTestId("tilføj-punkt")).toBeTruthy(); // data-testid="tilføj-punkt"
     });
 
-    // Uden currentUser, clicker tilføj uden punkt (fejl)
-    it("renders <TilføjPunkt /> UDEN en currentUser og clicker tilføj-punkt-button UDEN punkt", () => {
+    // Uden currentUser, clicker tilføj uden "punkt" (fejl)
+    it("renders <TilføjPunkt /> UDEN currentUser og clicker tilføj-punkt-button UDEN punkt", () => {
       const theme = {};
       const dark = true;
       const toggle = jest.fn();
@@ -78,8 +80,8 @@ describe("<TilføjPunkt />", () => {
       });
     });
 
-    // Uden currentUser, tilføjer punkt (fejl)
-    it("renders <TilføjPunkt /> UDEN en currentUser og clicker tilføj-punkt-button MED punkt", () => {
+    // Uden currentUser, tilføjer punkt med "punkt" (fejl)
+    it("renders <TilføjPunkt /> UDEN currentUser og clicker tilføj-punkt-button MED punkt", () => {
       const theme = {};
       const dark = true;
       const toggle = jest.fn();
@@ -127,7 +129,7 @@ describe("<TilføjPunkt />", () => {
     });
 
     // Med currentUser, tilføjer punkt via enter og resolver promise (firebase mock i toppen.)
-    it("renders <TilføjPunkt /> MED en currentUser og tilføjer punkt med Enter-key", () => {
+    it("renders <TilføjPunkt /> MED currentUser og tilføjer punkt med Enter-key", () => {
       const theme = {};
       const dark = true;
       const toggle = jest.fn();
@@ -139,28 +141,10 @@ describe("<TilføjPunkt />", () => {
         // emailVerified: true,
       };
       const { queryByTestId } = render(
-        <AuthContext.Provider
-          value={{
-            currentUser
-          }}
-        >
-          <ListerContext.Provider
-            value={{
-              lister
-            }}
-          >
-            <ValgtListeContext.Provider
-              value={{
-                valgtListe
-              }}
-            >
-              <ThemeContext.Provider
-                value={{
-                  theme,
-                  dark,
-                  toggle
-                }}
-              >
+        <AuthContext.Provider value={{ currentUser }}>
+          <ListerContext.Provider value={{ lister }}>
+            <ValgtListeContext.Provider value={{ valgtListe }}>
+              <ThemeContext.Provider value={{ theme, dark, toggle }}>
                 <TilføjPunkt />
               </ThemeContext.Provider>
             </ValgtListeContext.Provider>
@@ -168,30 +152,70 @@ describe("<TilføjPunkt />", () => {
         </AuthContext.Provider>
       );
 
-      // Ændrer staten i input feltet (skriver noget...)
-      fireEvent.change(queryByTestId("tilføj-punkt-input"), {
-        target: {
-          value: "nyt punkt" // Input har ændret sig
-        }
+      act(() => {
+        // Ændrer staten i input feltet (skriver noget...)
+        fireEvent.change(queryByTestId("tilføj-punkt-input"), {
+          target: {
+            value: "nyt punkt" // Input har ændret sig
+          }
+        });
       });
       expect(queryByTestId("tilføj-punkt-input").value).toBe("nyt punkt"); // data-testid="tilføj-punkt-input"
 
-      // Clicker på tilføj knappen (virker)
-      fireEvent.click(queryByTestId("tilføj-punkt-button")); //CLICK
-      fireEvent.keyDown(queryByTestId("tilføj-punkt-button")); // KEYDOWN
+      act(() => {
+        // Enter keyPress submit
+        fireEvent.keyPress(queryByTestId("tilføj-punkt-input"), {
+          key: "Enter",
+          code: 13,
+          charCode: 13
+        });
 
-      // Enter keyPress submit
-      fireEvent.keyPress(queryByTestId("tilføj-punkt-input"), {
-        key: "Enter",
-        code: 13,
-        charCode: 13
+        // else path på keyDown (!Enter)
+        fireEvent.keyPress(queryByTestId("tilføj-punkt-input"), {
+          key: "0",
+          code: 48, // 48 = "0"
+          charCode: 48 // 48 = "0"
+        });
       });
+    });
 
-      // else path på keyDown (!Enter)
-      fireEvent.keyPress(queryByTestId("tilføj-punkt-input"), {
-        key: "0",
-        code: 48, // 48 = "0"
-        charCode: 48 // 48 = "0"
+    // Med currentUser, tilføjer punkt via onKeyDown og resolver promise (firebase mock i toppen.)
+    it("renders <TilføjPunkt /> MED currentUser og tilføjer punkt via onKeyDOwn", () => {
+      const theme = {};
+      const dark = true;
+      const toggle = jest.fn();
+      const lister = [{}];
+      const valgtListe = "someListe";
+      const currentUser = {
+        email: "user@email.com"
+        // updateProfile: jest.fn(),
+        // emailVerified: true,
+      };
+
+      const { queryByTestId } = render(
+        <AuthContext.Provider value={{ currentUser }}>
+          <ListerContext.Provider value={{ lister }}>
+            <ValgtListeContext.Provider value={{ valgtListe }}>
+              <ThemeContext.Provider value={{ theme, dark, toggle }}>
+                <TilføjPunkt />
+              </ThemeContext.Provider>
+            </ValgtListeContext.Provider>
+          </ListerContext.Provider>
+        </AuthContext.Provider>
+      );
+
+      act(() => {
+        // Ændrer staten i input feltet (skriver noget...)
+        fireEvent.change(queryByTestId("tilføj-punkt-input"), {
+          target: {
+            value: "nyt punkt" // Input har ændret sig
+          }
+        });
+      });
+      expect(queryByTestId("tilføj-punkt-input").value).toBe("nyt punkt"); // data-testid="tilføj-punkt-input"
+
+      act(() => {
+        fireEvent.keyDown(queryByTestId("tilføj-punkt-button")); // KEYDOWN
       });
     });
   });
