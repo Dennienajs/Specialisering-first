a7bf251f586ee4a8454bf0d71f82db7cb95eb04b
Dennienajs <dennie.najs@hotmail.com>
2019-12-16 16:11:25 +0100

    Fixed redirect to login when no current user

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -4,7 +4,7 @@ import { AuthContext } from "./context";
 
 // Hvilken route som skal renders, hvis der er en auththenticated user (Content component)
 export const PrivateRoute = ({ component: RouteComponent, ...rest }: any) => {
-  const currentUser = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
   return (
     <Route
